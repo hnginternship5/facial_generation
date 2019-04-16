93111393ed4664e9047027e5b600437068433a6b
Nedu Robert <nedumrobert@gmail.com>
2019-04-16 16:55:07 +0100

    Feat: cli and genwhite

---
+++ cli.py
@@ -0,0 +1,33 @@
+import click
+#import genblack
+import genwhite
+import os
+
+@click.command()
+@click.argument('age')
+@click.argument('region')
+@click.argument('sex')
+def main(age,region,sex):
+    data_dir='/images'
+    # Image configuration
+    IMAGE_HEIGHT = 28
+    IMAGE_WIDTH = 28
+    
+    batch_size = 16
+    z_dim = 100
+    learning_rate = 0.0002
+    beta1 = 0.5
+    epochs = 2
+    if region=='black':
+        pass
+        #genblack.train(epochs, batch_size, z_dim, learning_rate, beta1, get_batches, shape)
+    elif region=='white':
+        data_files = genwhite.glob(os.path.join(data_dir, 'white/*.jpg'))
+        shape = len(data_files), IMAGE_WIDTH, IMAGE_HEIGHT, 3
+        genwhite.train(epochs, batch_size, z_dim, learning_rate, beta1, genwhite.get_batches,shape)
+    else:
+        click.echo("Enter either black or white as region")
+
+if __name__=='__main__':
+    main()
+    
