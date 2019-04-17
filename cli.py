f68abf019e2cfe04681c6a99e237f54051c92c83
LPMatrix <mubaraqsanusi908@gmail.com>
2019-04-17 08:45:38 +0100

    modifications

--- cli.py
+++ cli.py
@@ -18,17 +18,42 @@ def main(age,region,sex):
     z_dim = 100
     learning_rate = 0.0002
     beta1 = 0.5
-    epochs = 2
+    epochs = 20
     if region=='black':
-        pass
-        #genblack.train(epochs, batch_size, z_dim, learning_rate, beta1, get_batches, shape)
+        if sex == 'female':
+            data_files = genwhite.glob(os.path.join(data_dir, 'black/female/*.jpg'))
+            data_files.extend(genwhite.glob('*.png'))
+            shape = len(data_files), IMAGE_WIDTH, IMAGE_HEIGHT, 3
+            with tf.Session() as sess:
+                sess.run(tf.global_variables_initializer())
+            with tf.Graph().as_default():
+                genwhite.train(epochs, batch_size, z_dim, learning_rate, beta1,shape)
+        elif sex == 'male':
+            data_files = genwhite.glob(os.path.join(data_dir, 'black/male/*.jpg'))
+            data_files.extend(genwhite.glob('*.png'))
+            shape = len(data_files), IMAGE_WIDTH, IMAGE_HEIGHT, 3
+            with tf.Session() as sess:
+                sess.run(tf.global_variables_initializer())
+            with tf.Graph().as_default():
+                genwhite.train(epochs, batch_size, z_dim, learning_rate, beta1,shape)
+
     elif region=='white':
-        data_files = genwhite.glob(os.path.join(data_dir, 'white/*.jpg'))
-        shape = len(data_files), IMAGE_WIDTH, IMAGE_HEIGHT, 3
-        with tf.Session() as sess:
-            sess.run(tf.global_variables_initializer())
-        with tf.Graph().as_default():
-            genwhite.train(epochs, batch_size, z_dim, learning_rate, beta1,shape)
+        if sex == 'female':
+            data_files = genwhite.glob(os.path.join(data_dir, 'white/female/*.jpg'))
+            data_files.extend(genwhite.glob('*.png'))
+            shape = len(data_files), IMAGE_WIDTH, IMAGE_HEIGHT, 3
+            with tf.Session() as sess:
+                sess.run(tf.global_variables_initializer())
+            with tf.Graph().as_default():
+                genwhite.train(epochs, batch_size, z_dim, learning_rate, beta1,shape)
+        elif sex == 'male':
+            data_files = genwhite.glob(os.path.join(data_dir, 'white/male/*.jpg'))
+            data_files.extend(genwhite.glob('*.png'))
+            shape = len(data_files), IMAGE_WIDTH, IMAGE_HEIGHT, 3
+            with tf.Session() as sess:
+                sess.run(tf.global_variables_initializer())
+            with tf.Graph().as_default():
+                genwhite.train(epochs, batch_size, z_dim, learning_rate, beta1,shape)
     else:
         click.echo("Enter either black or white as region")
 
